7a9424025f73657398cc6e8f29aada3a1264f46b
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-03-08 16:07:51 +0000

    remap redis port

--- conf/settings.py
+++ conf/settings.py
@@ -112,8 +112,8 @@ ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
 # -----------------------------------------------------------------------------
 # Celery
 # -----------------------------------------------------------------------------
-CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="redis://redis:6379")
-CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND", default="redis://redis:6379")
+CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="redis://redis:6380")
+CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND", default="redis://redis:6380")
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
