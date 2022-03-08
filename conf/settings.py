18ae33d73019a2f6df9f7c89e41b944f378c4671
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2022-03-08 00:53:07 +0000

    example

--- conf/settings.py
+++ conf/settings.py
@@ -130,7 +130,6 @@ EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
 EMAIL_PORT = env.int("EMAIL_PORT")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
 
 # -----------------------------------------------------------------------------
 # Rest Framework
