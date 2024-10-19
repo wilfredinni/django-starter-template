1f2f574836634d65f5525fa0f50f671c921bcf31
Carlos <carlos@pythoncheatsheet.org>
2024-10-19 14:00:28 -0300

    feat> restart template

--- apps/users/models.py
+++
@@ -1,9 +0,0 @@
-# from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class CustomUser(AbstractUser):
-    # more fields here
-
-    def __str__(self):
-        return f"{self.username}"
