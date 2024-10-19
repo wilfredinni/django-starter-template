1e4e8eea9ee806b48d1f8932b8a33bcc61702961
Carlos <carlos@pythoncheatsheet.org>
2024-10-19 18:23:30 +0000

    feat: configure project urls

--- conf/settings.py
+++ conf/settings.py
@@ -40,6 +40,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+
 # -----------------------------------------------------------------------------
 # Databases
 # -----------------------------------------------------------------------------
