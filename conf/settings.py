423b33e9af39db973cc9e932f04847e843e6e699
Carlos <carlos@pythoncheatsheet.org>
2025-01-02 23:04:17 +0000

    fix: add default storage configuration in settings

--- conf/settings.py
+++ conf/settings.py
@@ -232,6 +232,9 @@ if not DEBUG:
 # Static & Media Files
 # -----------------------------------------------------------------------------
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
