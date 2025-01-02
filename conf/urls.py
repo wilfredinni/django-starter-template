6ddb5a38a10f4bbdda425cace6e1dd20b0ad7cee
Carlos <carlos@pythoncheatsheet.org>
2025-01-02 23:08:56 +0000

    chore: bump version to 0.1.9

--- conf/urls.py
+++ conf/urls.py
@@ -12,7 +12,7 @@ def index(request):
         "https://github.com/wilfredinni/django-starter-template?tab=readme-ov-file"
     )
     context = {}
-    context["version"] = "0.1.8"
+    context["version"] = "0.1.9"
     context["buttons"] = [
         {"title": "🚀 Features", "url": f"{base_url}#key-features"},
         {"title": "📋 Requirements", "url": f"{base_url}#requirements"},
