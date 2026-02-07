5e98206c4708be00597189fc09af0b62b079efb8
Carlos <carlos@pythoncheatsheet.org>
2026-02-07 14:53:27 -0300

    fix: update CELERY_TIMEZONE setting from America/Santiago to UTC

--- conf/settings.py
+++ conf/settings.py
@@ -214,7 +214,7 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers.DatabaseScheduler"
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
-CELERY_TIMEZONE = "America/Santiago"
+CELERY_TIMEZONE = "UTC"
 CELERY_RESULT_EXTENDED = True
 
 # -----------------------------------------------------------------------------
