a02c708e20b2bba59de2c204dc3924b83a6a8cca
Carlos <carlos@pythoncheatsheet.org>
2026-02-07 14:44:35 -0300

    refactor: remove duplicated XFrameOptionsMiddleware from MIDDLEWARE settings

--- conf/settings.py
+++ conf/settings.py
@@ -104,7 +104,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 
